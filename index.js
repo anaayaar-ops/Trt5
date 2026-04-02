24964ac953d14b0f2c66e7509b393d97321de1fd
anaayaar-ops <ana.ayaar@gmail.com>
2026-04-02 21:37:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const settings = {
     secret: process.env.U_PASS,
     targetGroupId: 18432094, // رقم القناة (الغرفة)
     commandToSend: "!مد مهام", // الأمر المطلوب إرساله
-    intervalDuration: 60 * 1000 // دقيقة واحدة (60 ثانية)
+    intervalDuration: 61 * 1000 // دقيقة واحدة (60 ثانية)
 };
 
 const service = new WOLF();
