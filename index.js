cd4e4a59f687bc9c593a5379c48b79d0ca0fcb78
anaayaar-ops <ana.ayaar@gmail.com>
2026-04-16 05:43:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const settings = {
     identity: process.env.U_MAIL || 'your_email@example.com',
     secret: process.env.U_PASS || 'your_password',
     targetGroupId: 330865 , 
-    minuteInterval: 63 * 1000,
+    minuteInterval: 303 * 1000,
     boxInterval: 3 * 60 * 1000
 };
 
